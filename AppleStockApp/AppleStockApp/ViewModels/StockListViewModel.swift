5b786731aec1ad87ada75706ad7a00c4e783bfbe
brightsummit.co <naseemoyebola@gmail.com>
2021-11-26 22:16:41 +0100

    StockViewModel

---
+++ AppleStockApp/AppleStockApp/ViewModels/StockListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  StockListViewModel.swift
+//  AppleStockApp
+//
+//  Created by naseem on 26/11/2021.
+//
+
+import Foundation
+
+struct StockListViewModel {
+    
+    var stocks: [StockViewModel] = [StockViewModel]()
+    
+}
---
+++ AppleStockApp/AppleStockApp/ViewModels/StockViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  StockViewModel.swift
+//  AppleStockApp
+//
+//  Created by naseem on 26/11/2021.
+//
+
+import Foundation
+
+struct StockViewModel {
+    let stock: Stock
+    
+    var symbol: String {
+        return self.stock.symbol.uppercased()
+    }
+    
+    var description: String {
+        return self.stock.description
+    }
+    
+    var price: String {
+        return String(format: "%.2f", self.stock.price)
+    }
+}
